bdf1ed76da5184bf6956389af1b6f2c6f66c105f
Feross Aboukhadijeh <feross@feross.org>
2016-06-15 22:57:43 -0700

    use port 9200 in prod

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ exports.isProd = process.env.NODE_ENV === 'production'
 exports.host = exports.isProd && '23.239.22.146'
 
 exports.ports = {
-  http: exports.isProd ? 80 : 9200,
-  https: exports.isProd ? 443 : 9201
+  http: 9200,
+  https: 9201
 }
 
 exports.rootPath = __dirname
