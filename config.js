4a452d6a7f63a328d0af1d1ef719a97139976004
Feross Aboukhadijeh <feross@feross.org>
2016-06-15 22:59:56 -0700

    listen on localhost

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var path = require('path')
 
 exports.isProd = process.env.NODE_ENV === 'production'
 
-exports.host = exports.isProd && '23.239.22.146'
+// exports.host = exports.isProd && '23.239.22.146'
 
 exports.ports = {
   http: 9200,
